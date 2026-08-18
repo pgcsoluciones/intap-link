c7817555d4cfa6f4892242ad24da15c393ea0493
pgcsoluciones <fliaprince@gmail.com>
2026-08-18 03:35:37 -0400

    feat: register free feedback super admin endpoint

--- api/src/preview-free-entry.ts
+++ api/src/preview-free-entry.ts
@@ -2,6 +2,7 @@ import './preview-starter-apply'
 import './preview-support-tickets'
 import './preview-exit-feedback'
 import './free-profile-preview'
+import './free-feedback'
 import app from './preview-free-actions'
 
 export default app
