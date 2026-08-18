5e12a7ceee40bdd1207c50a42511e444ef4f3594
pgcsoluciones <fliaprince@gmail.com>
2026-08-18 03:34:28 -0400

    fix: register authenticated profile preview proxy

--- api/src/preview-free-entry.ts
+++ api/src/preview-free-entry.ts
@@ -1,6 +1,7 @@
 import './preview-starter-apply'
 import './preview-support-tickets'
 import './preview-exit-feedback'
+import './free-profile-preview'
 import app from './preview-free-actions'
 
 export default app
