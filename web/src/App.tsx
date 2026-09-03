5e4a6583ad25b13776205758cbbffe74012624bc
pgcsoluciones <fliaprince@gmail.com>
2026-09-02 23:37:31 -0400

    feat(web): load polished unavailable profile styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,6 +14,7 @@ import {
 } from 'react-router-dom'
 
 import PublicBankAccounts from './components/free-profile/PublicBankAccounts'
+import './components/PublicProfileError.css'
 
 /*
  * IMPORTANTE
